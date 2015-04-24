5f1637546bf55e4f9cc33b9663f3dbfafc65bcd0
Fukuo Kadota <app.fk91@gmail.com>
2015-04-24 23:16:43 +0900

    Add gem_list

---
+++ gem_template.rb
@@ -0,0 +1,31 @@
+gem_group :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'faker'
+end
+
+gem_group :development do
+  gem 'better_errors'
+end
+
+gem_group :test, :development do
+  gem 'sqlite3'
+  gem 'byebug'
+  gem 'pry-byebug'
+  gem 'pry-doc'
+  gem 'hirb'
+  gem 'hirb-unicode'
+  gem 'awesome_print'
+end
+
+gem 'pry'
+gem 'pry-rails'
+
+gem 'activeadmin', '~> 1.0.0.pre1'
+gem 'devise', '~> 3.4.1'
+gem 'omniauth', '~> 1.2.1'
+gem 'omniauth-facebook', '~>1.6.0'
+
+gem 'slim', '~> 3.0.3'
+gem 'slim-rails', '~> 3.0.1'

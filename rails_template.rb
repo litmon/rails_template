322a58417e4f52659b0b358a14e2c657765ab718
Fukuo Kadota <app.fk91@gmail.com>
2015-05-24 19:45:14 +0900

    Load gem from gems.yml

--- rails_template.rb
+++ rails_template.rb
@@ -1,1 +1,12 @@
+require 'yaml'
+require 'ap'
 
+confs = YAML.load_file("gems.yml")
+
+confs.each do |conf|
+  gem_group conf['environment'].map{|s| s.to_sym} do
+    conf['gems'].each do |gem_name|
+      gem gem_name
+    end
+  end
+end
